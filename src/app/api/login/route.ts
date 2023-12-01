d2219eef6f277ddbecc7212e8868cf77a386fc42
loopzer <loopzer@gmail.com>
2023-12-02 00:17:42 +0100

    Changed postgres lib to pg

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,23 +1,20 @@
 import { Say } from '@/logica/say'
-import { Client } from 'ts-postgres';
+import { Pool } from 'pg'
 import { v4 as uuidv4 } from 'uuid';
 
 
+const pool = new Pool({
+  user: 'gamers',
+  host: '127.0.0.1',
+  database: 'gamers',
+  password: 'password',
+  port: 5432,
+})
 
 export async function GET(request: Request) {
 
-  const client = new Client({
-    host: '127.0.0.1',
-    user: 'gamers',
-    password: 'password',
-    database: 'gamers'
-  });
-
-  await client.connect();
-
-  const rows = (await client.query('SELECT * FROM users')).rows;
+  const rows = (await pool.query('SELECT * FROM users')).rows;
   
-  client.end();
   return Response.json(rows)
 }
 
@@ -25,18 +22,8 @@ export async function POST(request: Request) {
 
   const data = await request.json();
   console.log(data)
-  
-  const client = new Client({
-    host: '127.0.0.1',
-    user: 'gamers',
-    password: 'password',
-    database: 'gamers'
-  });
 
-  await client.connect();
-
-  const rows = (await client.query('SELECT * FROM users')).rows;
+  const rows = (await pool.query('SELECT * FROM users')).rows;
   
-  client.end();
   return Response.json(rows)
 }
