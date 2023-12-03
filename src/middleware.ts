2a435c804deb151a1cb5d1a5dbb305628c1da6aa
loopzer <loopzer@gmail.com>
2023-12-03 02:01:17 +0100

    Ignore files middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,31 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-import { publicRoutes } from './app/routes'
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { publicRoutes } from "../src/app/routes";
 import { verify } from '@/backend/auth/authUtility'
 
+
 export async function middleware(request: NextRequest) {
-	if (publicRoutes.includes(request.nextUrl.pathname)) {
-		console.log('public:' + request.nextUrl.pathname)
-		return
-	}
+  
 
-	const token = request.headers.get('authorization') || ''
+  if(request.nextUrl.pathname.startsWith("/_next/static/") 
+  || request.nextUrl.pathname.endsWith(".js")
+  || request.nextUrl.pathname.endsWith(".css")
+  || request.nextUrl.pathname.endsWith(".svg")
+  || request.nextUrl.pathname.endsWith(".png")
+  || request.nextUrl.pathname.endsWith(".ico") )
+  {
+    return;
+  }
 
-	try {
-		await verify(token)
-	} catch (error) {
-		return new NextResponse('401 Unauthorized', { status: 401 })
-	}
+  if (publicRoutes.includes(request.nextUrl.pathname)) {
+    return;
+  }
+
+  const token = request.headers.get("authorization") || '';
+
+  try {
+    const data = await verify(token);
+  } catch (error) {
+    return new NextResponse('401 Unauthorized', { status: 401 });
+  }
 }
